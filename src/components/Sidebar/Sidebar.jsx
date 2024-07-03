dc49a3d5359f6ad2cbc0dcd80b335bab2c7bc180
MustafoAlishrovich <darklord7133@gmail.com>
2024-07-04 02:49:51 +0500

    update main and sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,45 +3,45 @@ import './Sidebar.css'
 import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
-
-    const [extendend, setExtendend] = useState(false)
+    const [extended, setExtended] = useState(false);
 
     return (
-        <div className='sidebar'>
+        <div className={`sidebar ${extended ? '' : 'collapsed'}`}>
             <div className="top">
-                <img onClick={()=>setExtendend(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" />
-                <div className="new-chat">
-                    <img src={assets.plus_icon} alt="" />
-                    {extendend ? <p>New Chat</p> : null}
+                <img 
+                    onClick={() => setExtended(prev => !prev)} 
+                    className='menu' 
+                    src={assets.menu_icon} 
+                    alt="Menu" 
+                />
+                <div className={`new-chat ${extended ? '' : 'collapsed'}`}>
+                    <img src={assets.plus_icon} alt="New Chat" />
+                    {extended ? <p>New Chat</p> : null}
                 </div>
-                {extendend
-                    ?
-                    <div className="recent">
-                        <p className="recent-title">Recent</p>
-                        <div className="recent-entry">
-                            <img src={assets.message_icon} alt="" />
-                            <p>What is react...</p>
-                        </div>
+                <div className={`recent ${extended ? '' : 'collapsed'}`}>
+                    <p className="recent-title">Recent</p>
+                    <div className={`recent-entry ${extended ? '' : 'collapsed'}`}>
+                        <img src={assets.message_icon} alt="Message" />
+                        {extended ? <p>What is react...</p> : null}
                     </div>
-                    : null
-                }
-
+                </div>
             </div>
             <div className="bottom">
-                <div className="bottom-item recent-entry">
-                    <img src={assets.question_icon} alt="" />
-                    {extendend ? <p>Help</p> : null}
+                <div className={`bottom-item recent-entry ${extended ? '' : 'collapsed'}`}>
+                    <img src={assets.question_icon} alt="Help" />
+                    {extended ? <p>Help</p> : null}
                 </div>
-                <div className="bottom-item recent-entry">
-                    <img src={assets.history_icon} alt="" />
-                    {extendend ? <p>Activity</p> : null}
-                </div><div className="bottom-item recent-entry">
-                    <img src={assets.setting_icon} alt="" />
-                    {extendend ? <p>Settings</p> : null}
+                <div className={`bottom-item recent-entry ${extended ? '' : 'collapsed'}`}>
+                    <img src={assets.history_icon} alt="Activity" />
+                    {extended ? <p>Activity</p> : null}
+                </div>
+                <div className={`bottom-item recent-entry ${extended ? '' : 'collapsed'}`}>
+                    <img src={assets.setting_icon} alt="Settings" />
+                    {extended ? <p>Settings</p> : null}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
